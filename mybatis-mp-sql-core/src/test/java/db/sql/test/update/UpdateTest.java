64c10133bc9d7ecf6cea0719df4157cd7fbc82c2
WFD <wangfudong@aiyingli.com>
2023-10-27 16:13:23 +0800

    添加测试用例

---
+++ mybatis-mp-sql-core/src/test/java/db/sql/test/update/UpdateTest.java
@@ -0,0 +1,20 @@
+package db.sql.test.update;
+
+
+import db.sql.core.api.cmd.executor.Update;
+import db.sql.test.BaseTest;
+import org.junit.jupiter.api.Test;
+
+public class UpdateTest extends BaseTest {
+
+    @Test
+    void deleteTest() {
+        check("update 简单", "update user set name='xx' where id=1",
+                new Update().update(userTable()).set(userTable().$("name"), "xx").eq(userTable().$("id"), 1)
+        );
+
+        check("update 别名", "update user t set t.name='xx' where t.id=1",
+                new Update().update(userTable().as("t")).set(userTable().as("t").$("name"), "xx").eq(userTable().as("t").$("id"), 1)
+        );
+    }
+}

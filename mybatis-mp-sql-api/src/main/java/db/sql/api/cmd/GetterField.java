5b7403532488812da687df19b2e2ebaca4bcef38
Ai东 <wangfudong@aiyingli.com>
2023-12-21 11:18:40 +0800

    增加多个字段动态条件方法

---
+++ mybatis-mp-sql-api/src/main/java/db/sql/api/cmd/GetterField.java
@@ -0,0 +1,35 @@
+package db.sql.api.cmd;
+
+import db.sql.api.Getter;
+
+public class GetterField<T> {
+
+    public static <T> GetterField create(Getter<T> getter) {
+        return create(getter, 1);
+    }
+
+    public static <T> GetterField create(Getter<T> getter, int storey) {
+        return new GetterField(getter, storey);
+    }
+
+    private final Getter<T> getter;
+
+    private final int storey;
+
+    public GetterField(Getter<T> getter) {
+        this(getter, 1);
+    }
+
+    public GetterField(Getter<T> getter, int storey) {
+        this.getter = getter;
+        this.storey = storey;
+    }
+
+    public Getter<T> getGetter() {
+        return getter;
+    }
+
+    public int getStorey() {
+        return storey;
+    }
+}
---
+++ mybatis-mp-sql-api/src/main/java/db/sql/api/cmd/struct/conditionChain/IConditionChainAnd.java
@@ -0,0 +1,55 @@
+package db.sql.api.cmd.struct.conditionChain;
+
+import db.sql.api.Getter;
+import db.sql.api.cmd.GetterField;
+import db.sql.api.cmd.basic.ICondition;
+
+import java.util.function.Function;
+
+public interface IConditionChainAnd<SELF extends IConditionChainAnd, TABLE_FIELD> {
+
+    SELF and();
+
+    SELF and(ICondition condition);
+
+    default SELF and(ICondition condition, boolean when){
+        if(!when){
+            return (SELF)this;
+        }
+        return this.and(condition);
+    }
+
+    default <T> SELF and(Getter<T> column, Function<TABLE_FIELD, ICondition> function) {
+        return this.and(true, column, function);
+    }
+
+    default <T> SELF and(boolean when, Getter<T> column, Function<TABLE_FIELD, ICondition> function) {
+        return this.and(when, column, 1, function);
+    }
+
+    default <T> SELF and(Getter<T> column, int storey, Function<TABLE_FIELD, ICondition> function) {
+        return this.and(true, column, storey, function);
+    }
+
+    <T> SELF and(boolean when, Getter<T> column, int storey, Function<TABLE_FIELD, ICondition> function);
+
+    default <T> SELF and(Function<TABLE_FIELD[], ICondition> function, Getter<T>... columns) {
+        return this.and(true, function, columns);
+    }
+
+    default <T> SELF and(boolean when, Function<TABLE_FIELD[], ICondition> function, Getter<T>... columns) {
+        return this.and(when, function, 1, columns);
+    }
+
+    default <T> SELF and(Function<TABLE_FIELD[], ICondition> function, int storey, Getter<T>... columns) {
+        return this.and(true, function, 1, columns);
+    }
+
+    <T> SELF and(boolean when, Function<TABLE_FIELD[], ICondition> function, int storey, Getter<T>... columns);
+
+    default SELF and(Function<TABLE_FIELD[], ICondition> function, GetterField... getterFields) {
+        return and(true, function, getterFields);
+    }
+
+    SELF and(boolean when, Function<TABLE_FIELD[], ICondition> function, GetterField... getterFields);
+}

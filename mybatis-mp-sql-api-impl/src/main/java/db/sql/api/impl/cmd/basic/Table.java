3e38894d822ae14a9135d8c6cb9b2243fec4de6f
Ai东 <wangfudong@aiyingli.com>
2023-12-27 11:04:36 +0800

    增加force Index功能

--- mybatis-mp-sql-api-impl/src/main/java/db/sql/api/impl/cmd/basic/Table.java
+++ mybatis-mp-sql-api-impl/src/main/java/db/sql/api/impl/cmd/basic/Table.java
@@ -3,7 +3,11 @@ package db.sql.api.impl.cmd.basic;
 
 import db.sql.api.Cmd;
 import db.sql.api.SqlBuilderContext;
+import db.sql.api.impl.cmd.struct.From;
+import db.sql.api.impl.cmd.struct.Join;
 import db.sql.api.impl.tookit.SqlConst;
+
+import java.util.Objects;
 
 public class Table implements Dataset<Table, TableField> {
 
@@ -12,6 +16,8 @@ public class Table implements Dataset<Table, TableField> {
     protected String alias;
 
     protected String prefix;
+
+    private String forceIndex;
 
     public Table(String name) {
         this.name = name;
@@ -40,6 +46,7 @@ public class Table implements Dataset<Table, TableField> {
         return as(alias);
     }
 
+
     public Table as(String alias) {
         this.alias = alias;
         return this;
@@ -60,11 +67,21 @@ public class Table implements Dataset<Table, TableField> {
         if (getAlias() != null) {
             sqlBuilder.append(SqlConst.BLANK).append(getAlias());
         }
+        if (parent instanceof From || parent instanceof Join) {
+            if (Objects.nonNull(forceIndex) && !SqlConst.EMPTY.equals(forceIndex)) {
+                sqlBuilder.append(SqlConst.FORCE_INDEX).append(SqlConst.BRACKET_LEFT).append(forceIndex).append(SqlConst.BRACKET_RIGHT);
+            }
+        }
         return sqlBuilder;
     }
 
     @Override
     public boolean contain(Cmd cmd) {
         return false;
+    }
+
+    public Table forceIndex(String forceIndex) {
+        this.forceIndex = forceIndex;
+        return this;
     }
 }
